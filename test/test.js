2759a10fe32ac1fa0e33911af6808e43a931ea64
Gonzalo Matheu <gmatheu@thoughtworks.com>
2015-04-21 17:52:49 -0300

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var assert = require('assert')
+var should = require('should');
+var fizzBuzz = require('../lib/fizzBuzz');
+
+describe('FizzBuzz', function() {
+  describe('#process', function() {
+      it('should return same input', function() {
+        fizzBuzz.process([1]).should.be[1]
+      })
+    })
+})
