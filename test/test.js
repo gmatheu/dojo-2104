18202529f66323126d119d2a08432f00ea94cd20
Gonzalo Matheu <gmatheu@thoughtworks.com>
2015-04-21 20:07:00 -0300

    More tests...

--- test/test.js
+++ test/test.js
@@ -11,5 +11,21 @@ describe('FizzBuzz', function() {
       it('when input is 3 return Fizz', function() {
         fizzBuzz.process(3).should.eql([1,2,"Fizz"]);
       })
+      it('when input is 5 return 1,2,Fizz,4,Buzz', function() {
+        fizzBuzz.process(5).should.eql([1,2,"Fizz",4,"Buzz"]);
+      })
+      it('when input is 6 return 1,2,Fizz,4,Buzz,Fizz', function() {
+        fizzBuzz.process(6).should.eql([1,2,"Fizz",4,"Buzz","Fizz"]);
+      })
+      it('when input is 9 return 1,2,Fizz,4,Buzz,Fizz .... Fizz', function() {
+        fizzBuzz.process(9).should.eql([1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz"]);
+      })
+      it('when input is 15 return 1,2,Fizz,4,Buzz,Fizz .... Fizz', function() {
+        fizzBuzz.process(15).should.eql([1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz",11,"Fizz","Fizz",14,"FizzBuzz"]);
+      })
+      it('when the input has a 3, expect Fizz', function() {
+        fizzBuzz.process(13)[12].should.eql("Fizz");
+        fizzBuzz.process(23)[22].should.eql("Fizz");
+      })
     })
 })
